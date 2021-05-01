258ad43e45cea1a0619a4dcbbae922db314d3580
maritox09 <jmyc9999@gmail.com>
2021-04-30 23:06:43 -0600

    a

--- app.py
+++ app.py
@@ -363,7 +363,7 @@ def estadisticas():
     if verificar_permiso():
         clientes = db.clientes.find()
         for cliente in clientes:
-            json = {'id':int(cliente['id_ventas'])}
+            json = {'id':1}
             response = requests.post(str(cliente['url']) + "/api/ReporteVentas", json = json)
             if response.ok:
                 return response.json()
