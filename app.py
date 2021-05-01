985692cbb4f1989f09f85111d7e7f9738f996f1d
maritox09 <jmyc9999@gmail.com>
2021-05-01 00:57:30 -0600

    a

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def notificar_envio(orden):
     data = {"orden":orden}
     response = requests.post(url, json = data)
     if response.ok:
-        print response.text
+        print (response.text)
         return True
     else:
         return "nel"
